d571f3917da6c38f09745db6628d0a0908e207cb
Your Name <you@example.com>
2018-09-07 13:24:44 -0600

    updated mainstory board with label and button

---
+++ gethub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gethub
+//
+//  Created by Noah Martineau Raymundo on 2018-09-07.
+//  Copyright © 2018 Noah Martineau Raymundo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
